6e4a438ebb576829978fc86fac17cc5f0e0c8a07
joycehwchan <joycehwchan@gmail.com>
2023-01-16 14:10:15 +0900

    fetch song data

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -11,7 +11,7 @@ export const shazamCoreApi = createApi({
     },
   }),
   endpoints: (builder) => ({
-    getTopCharts: builder.query({ query: './charts/world' }),
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
   }),
 });
 
