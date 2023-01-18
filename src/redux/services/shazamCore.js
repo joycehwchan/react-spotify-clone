67623f85e86c829018ea710d979590a8f0482aa6
joycehwchan <joycehwchan@gmail.com>
2023-01-18 13:23:45 +0900

    display lyrics

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,18 +1,23 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+// prettier-ignore
 export const shazamCoreApi = createApi({
   reducerPath: 'shazamCoreApi',
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
     prepareHeaders: (headers) => {
-      headers.set('X-RapidAPI-Key', 'd4fd9bee5amsh0323acbf1961094p1cd7aajsnc444918fa3f9');
+      headers.set(
+        'X-RapidAPI-Key',
+        'd4fd9bee5amsh0323acbf1961094p1cd7aajsnc444918fa3f9',
+      );
 
       return headers;
     },
   }),
   endpoints: (builder) => ({
     getTopCharts: builder.query({ query: () => '/charts/world' }),
+    getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
   }),
 });
 
-export const { useGetTopChartsQuery } = shazamCoreApi;
+export const { useGetTopChartsQuery, useGetSongDetailsQuery } = shazamCoreApi;
